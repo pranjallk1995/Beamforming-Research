e29d66ae6c6b1e96077ec25f685f7c3b7c96d57a
pranjall.kumar <pranjall.kumar@siemensgamesa.com>
2021-12-15 16:15:17 +0530

    Dockerfile added

--- Environment/Lib/site-packages/jupyterlab_server/tests/test_listings_api.py
+++
@@ -1,8 +0,0 @@
-
-from .utils import validate_request
-
-
-async def test_get_listing(jp_fetch, labserverapp):
-    url = r"lab/api/listings/@jupyterlab/extensionmanager-extension/listings.json"
-    r = await jp_fetch(*url.split('/'))
-    validate_request(r)
